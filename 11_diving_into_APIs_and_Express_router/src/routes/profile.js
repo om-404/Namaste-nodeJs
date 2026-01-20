92eb10f8eaf1845f921cfafb3b657893403b8b57
Om Sonawale <omsonawale20212223@gmail.com>
2026-01-20 15:14:05 +0530

    profile api

--- 11_diving_into_APIs_and_Express_router/src/routes/profile.js
+++ 11_diving_into_APIs_and_Express_router/src/routes/profile.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const profileRouter = express.Router();
 const {userAuth} = require("../middlewares/auth.js");
+const {validateEditProfileData} = require("../utils/validation.js")
 
-profileRouter.get("/profile", userAuth, async (req, res) => {
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
 
     try {
         const user = req.user;
@@ -14,7 +15,30 @@ profileRouter.get("/profile", userAuth, async (req, res) => {
 
         } catch (error) {
             res.status(400).send("ERROR : " + error.message);
+    }
+})
+
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+
+    try {
+        if(!validateEditProfileData(req)){
+            throw new Error("Invalid edit request !");
         }
+
+        const loggedInUser = req.user;
+
+        Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
+
+        await loggedInUser.save();
+
+        res.json({
+            message: `${loggedInUser.firstName}, your profile updated successfully !`,
+            data: loggedInUser, 
+        });
+
+    } catch (error) {
+        res.status(400).send("ERROR : " + error.message);
+    }
 })
 
 module.exports = profileRouter;
--- 11_diving_into_APIs_and_Express_router/src/utils/validation.js
+++ 11_diving_into_APIs_and_Express_router/src/utils/validation.js
@@ -14,6 +14,25 @@ const validateSignupData = (req) => {
     } 
 };
 
+const validateEditProfileData = (req) => {
+    const allowedEditFields = [
+        "firstName",
+        "lastName",
+        "photoUrl",
+        "gender",
+        "age",
+        "about",
+        "skills"
+    ];
+
+    const isEditAllowed = Object.keys(req.body).every((field) => 
+        allowedEditFields.includes(field)
+    )
+
+    return isEditAllowed;
+}
+
 module.exports = {
-    validateSignupData
+    validateSignupData,
+    validateEditProfileData
 }
